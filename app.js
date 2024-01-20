cf1a11c7fd75955bf293de7c69e2530f1cd3b69c
Mauricio Abregú <mauricio.matias93@gmail.com>
2024-01-20 19:13:48 -0300

    Directory restructuring

--- app.js
+++
@@ -1,5 +0,0 @@
-import { marketDataExample } from "./src/classes/marketData.js";
-import { bmbSheetONs } from "./src/classes/gSheetONs.js";
-
-//bmbSheetONs.updateSheet();
-//console.log(marketData[0].lastPrice.ars.t48.value);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { marketDataExample } from "./classes/marketData.js";
+import { bmbSheetONs } from "./classes/gSheetONs.js";
+
+//bmbSheetONs.updateSheet();
+//console.log(marketData[0].lastPrice.ars.t48.value);
